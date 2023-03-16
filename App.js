bf804d4f680f50a34ca99555f6298024504db592
Jroutly3 <jroutly3@gatech.edu>
2023-03-16 12:01:42 -0400

    Creation of the basic calendar

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Calendar } from 'react-native-calendars';
+import dateFns from 'date-fns';
+
+
+
+const format = (date = new Date()) => dateFns.format(date, 'YYYY-MM-DD');
 
 export default function App() {
+  const baseDate = new Date(2022, 3, 16); //Will change to get date soon
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Calendar
+        onDayPress={(day) => {
+          console.log('selected day', day);
+        }}
+        current={format(baseDate)}
+        minDate={dateFns.subYears(baseDate, 1)}
+      />
     </View>
   );
 }
